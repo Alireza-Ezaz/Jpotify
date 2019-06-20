91e5e02f9c323626f2769260ab6bac3aec2a5832
9731009 <sar.ezaz@aut.ac.ir>
2019-06-20 16:57:33 +0430

    Creating logic class...

--- src/Logic/PlayList.java
+++ src/Logic/PlayList.java
@@ -1,4 +1,17 @@
 package Logic;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class PlayList {
+    private String name;
+    private ImageIcon imageIcon;
+    private ArrayList<Song> songs;
+
+    public PlayList(ArrayList<Song> songs) {
+        this.songs = songs;
+    }
+
+
+
 }
--- src/Logic/Song.java
+++ src/Logic/Song.java
@@ -1,4 +1,41 @@
 package Logic;
 
+import javax.swing.*;
+
+
 public class Song {
+    private String name;
+    private String directory;
+    private String artist;
+    private String album;
+    private ImageIcon artWork;
+
+    public Song(String directory) {
+        this.directory = directory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public ImageIcon getArtWork() {
+        return artWork;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nDirectory: " + directory + "\nArtist: " + artist + "\nAlbum: " + album;
+    }
 }
