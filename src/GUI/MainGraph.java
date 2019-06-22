396d9103204b356277b0491cf91778a04d4fc419
mohammadsadra <ms.haeri@aut.ac.ir>
2019-06-23 03:08:07 +0430

    left panel rewrited

--- src/GUI/MainGraph.java
+++ src/GUI/MainGraph.java
@@ -20,20 +20,20 @@ public class MainGraph {
         leftPanel.setPreferredSize(leftPanelDim);
 
         DefaultListModel listModelLibrary = new DefaultListModel();
-        listModelLibrary.addElement("Music");
-        listModelLibrary.addElement("Album");
-        listModelLibrary.addElement("Artist");
+        listModelLibrary.addElement(" Music");
+        listModelLibrary.addElement(" Album");
+        listModelLibrary.addElement(" Artist");
         JList libraryList = new JList(listModelLibrary);
 //        JScrollPane scrollPane = new JScrollPane();
-//        scrollPane.setViewportView(libraryList);
-//        libraryList.setLayoutOrientation(JList.VERTICAL);
+//        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+//        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
 
         DefaultListModel listModelplaylist = new DefaultListModel();//list of your all playlist
         JList playlistList = new JList(listModelplaylist);
 
         Dimension libListdim = new Dimension(150, 70);
-        Dimension playlisListdim = new Dimension(150, 150);
+        Dimension playlisListdim = new Dimension(150, 300);
         libraryList.setPreferredSize(libListdim);
         playlistList.setPreferredSize(playlisListdim);
 
@@ -46,7 +46,7 @@ public class MainGraph {
 
         leftPanel.add(library);
         leftPanel.add(libraryList);
-        //leftPanel.add(spane);
+        //leftPanel.add(scrollPane);
         leftPanel.add(playlistListLabel);
         leftPanel.add(playlistList);
 
@@ -64,41 +64,41 @@ public class MainGraph {
         artworkP.setPreferredSize(artworkMaxDim);
         leftPanel.add(artworkP);
 
+        JLabel musicName = new JLabel(" Music Name");
+        JLabel artist = new JLabel(" Artist Name");
+        musicName.setPreferredSize(LabelSize);
+        artist.setPreferredSize(LabelSize);
+        leftPanel.add(musicName);
+        leftPanel.add(artist);
+
+
         leftPanel.setBackground(Color.LIGHT_GRAY);
         myFrame.add(leftPanel, BorderLayout.WEST);
 
 
-
         //Center panel
-        JPanel centerPanel = new JPanel(new GridLayout(1, 1));
+        JPanel centerPanel = new JPanel(new FlowLayout());
         centerPanel.setBackground(Color.BLACK);
         myFrame.add(centerPanel, BorderLayout.CENTER);
 
         //East panel
-        JPanel eastPanel = new JPanel(new GridLayout(2, 1));
+        JPanel eastPanel = new JPanel(new FlowLayout());
+        eastPanel.setPreferredSize(leftPanelDim);
         eastPanel.setBackground(Color.GREEN);
-        eastPanel.add(new JLabel("Friend Activity"));
+        JLabel fActivity = new JLabel(" Friend Activity");
+        fActivity.setPreferredSize(LabelSize);
+        fActivity.setForeground(Color.WHITE);
+        eastPanel.add(fActivity);
         myFrame.add(eastPanel, BorderLayout.EAST);
 
-        //South panel
+        /////////South panel
         JPanel southPanel = new JPanel(new BorderLayout());
-        // west southpanel
-        JPanel musicinfo = new JPanel(new GridLayout(2, 1));
-        JLabel musicName = new JLabel("  Music  ");
-        JLabel artist = new JLabel("  Artist  ");
-        musicinfo.add(musicName);
-        musicinfo.add(artist);
-        musicinfo.setBackground(Color.lightGray);
-        southPanel.add(musicinfo, BorderLayout.WEST);
-        //east sothpanel
-        JSlider voloum = new JSlider();
-        southPanel.add(voloum, BorderLayout.EAST);
-        //south southpanel
-        JSlider musicSlider = new JSlider();
-        southPanel.add(musicSlider, BorderLayout.SOUTH);
-        // Center soutpanel
-        JPanel player = new JPanel(new GridLayout(1, 3));
+        JPanel playerPanel = new JPanel(new FlowLayout());
+        Dimension playerPanelSize = new Dimension(150, 100);
+
         JButton play = new JButton();
+        Dimension playerButtonSize = new Dimension(44, 44);
+        play.setPreferredSize(playerButtonSize);
         play.setBackground(Color.lightGray);
         try {
             Image img = ImageIO.read(new File("C:\\Users\\Mohammad Sadra\\IdeaProjects\\FP AP\\src\\pause.png"));//didnt work?!
@@ -108,17 +108,19 @@ public class MainGraph {
             System.out.println("Image not found");
         }
 
-        JButton next = new JButton();
-        next.setBackground(Color.lightGray);
+        JButton pNext = new JButton();
+        pNext.setPreferredSize(playerButtonSize);
+        pNext.setBackground(Color.lightGray);
         try {
             Image img = ImageIO.read(new File("C:\\Users\\Mohammad Sadra\\IdeaProjects\\FP AP\\src\\next.png"));//didnt work?!
-            next.setIcon(new ImageIcon(img));
+            pNext.setIcon(new ImageIcon(img));
 
         } catch (Exception ex) {
             System.out.println("Image not found");
         }
 
         JButton privios = new JButton();
+        privios.setPreferredSize(playerButtonSize);
         privios.setBackground(Color.lightGray);
         try {
             Image img = ImageIO.read(new File("C:\\Users\\Mohammad Sadra\\IdeaProjects\\FP AP\\src\\pr.png"));//didnt work?!
@@ -128,22 +130,52 @@ public class MainGraph {
             System.out.println("Image not found");
         }
 
-        player.add(privios);
-        player.add(play);
-        player.add(next);
-        southPanel.add(player, BorderLayout.CENTER);
+        playerPanel.add(privios);
+        playerPanel.add(play);
+        playerPanel.add(pNext);
+        playerPanel.setBackground(Color.lightGray);
+        southPanel.add(playerPanel, BorderLayout.WEST);
+
+
+        Dimension southPanelDim = new Dimension(Toolkit.getDefaultToolkit().getScreenSize().width, 100);
         southPanel.setBackground(Color.lightGray);
         myFrame.add(southPanel, BorderLayout.SOUTH);
 
 
         // north
-        JPanel northPanel = new JPanel(new GridLayout(1, 2));
-        northPanel.add(new JLabel("Add Music To Library"));
+        JPanel northPanel = new JPanel(new FlowLayout());
+        Dimension northPanelDim = new Dimension(Toolkit.getDefaultToolkit().getScreenSize().width, 30);
+        northPanel.setPreferredSize(northPanelDim);
+        JLabel add_music_to_libraryLabel = new JLabel("Add Music To Library");
+        Dimension addMtoL = new Dimension(150, 30);
+        add_music_to_libraryLabel.setPreferredSize(addMtoL);
+        northPanel.add(add_music_to_libraryLabel);
         JButton add = new JButton(" + ");
+        Dimension addLabel = new Dimension(60, 30);
+        add.setPreferredSize(addLabel);
         add.setBackground(Color.lightGray);
         northPanel.add(add);
         northPanel.setBackground(Color.lightGray);
         myFrame.add(northPanel, BorderLayout.NORTH);
+        JButton signIn = new JButton("Sign in");
+        signIn.setPreferredSize(addMtoL);
+        signIn.setBackground(Color.lightGray);
+        northPanel.add(signIn);
+        JLabel profileIcon = new JLabel();
+        try {
+            BufferedImage img = ImageIO.read(new File("C:\\Users\\Mohammad Sadra\\IdeaProjects\\FP AP\\src\\profile.jpg"));
+            BufferedImage fimg = new BufferedImage(30, 30, img.getType());
+            profileIcon.setIcon(new ImageIcon(fimg));
+
+        } catch (Exception ex) {
+            System.out.println("Image not found");
+        }
+        profileIcon.setPreferredSize(addLabel);
+        northPanel.add(profileIcon);
+        JCheckBox darkMode = new JCheckBox("Dark Mode");
+        darkMode.setPreferredSize(addMtoL);
+        darkMode.setBackground(Color.lightGray);
+        northPanel.add(darkMode);
 
 
         ///////////
