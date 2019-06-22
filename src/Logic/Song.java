2b616479566469e5eeaf332b2f138f809ee4d8d2
9731009 <sar.ezaz@aut.ac.ir>
2019-06-22 11:47:39 +0430

    Artist class useges removed

--- src/Logic/Song.java
+++ src/Logic/Song.java
@@ -6,16 +6,33 @@ import javax.swing.*;
 public class Song {
     private String name;
     private String directory;
-    private Artist artist;
+    private String artistName;
     private String albumName;
+    private String lyric ="not downloaded";
     private ImageIcon artWork;
 
     public Song(String directory) {
         this.directory = directory;
     }
 
-    public void setArtist(Artist artist) {
-        this.artist = artist;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setArtWork(ImageIcon artWork) {
+        this.artWork = artWork;
+    }
+
+    public void setLyric(String lyric) {
+        this.lyric = lyric;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
     }
 
     public String getName() {
@@ -30,16 +47,20 @@ public class Song {
         return albumName;
     }
 
-    public Artist getArtist() {
-        return artist;
+    public String getArtistName() {
+        return artistName;
     }
 
     public ImageIcon getArtWork() {
         return artWork;
     }
 
+    public String getLyric() {
+        return lyric;
+    }
+
     @Override
     public String toString() {
-        return "Name: " + name + "\nDirectory: " + directory + "\nArtist: " + artist.getName() + "\nAlbum: " + albumName;
+        return "Name: " + name + "\nDirectory: " + directory + "\nArtist: " + artistName + "\nAlbum: " + albumName;
     }
 }
