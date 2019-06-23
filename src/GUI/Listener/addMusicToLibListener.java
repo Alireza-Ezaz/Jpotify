d594af8138bea15bc47ccba1a9140a360891837d
mohammadsadra <ms.haeri@aut.ac.ir>
2019-06-24 03:59:50 +0430

    filechooser add to music add button

---
+++ src/GUI/Listener/addMusicToLibListener.java
@@ -0,0 +1,34 @@
+package GUI.Listener;
+
+import Logic.Library;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class addMusicToLibListener implements ActionListener {
+    private JFileChooser fileChooser;
+    private Library library;
+    private JFrame fileChooserFrame;
+
+    public addMusicToLibListener(Library library) {
+        fileChooser = new JFileChooser();
+        fileChooserFrame = new JFrame();
+        this.library = library;
+
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        fileChooserFrame.add(fileChooser);
+        fileChooserFrame.setVisible(true);
+        fileChooserFrame.setMinimumSize(new Dimension(600,400));
+        library.addSong(fileChooser.getSelectedFile().getPath());
+        fileChooserFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+}
