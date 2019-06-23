87306a10ba70f02db5c1639cfaca160a2182becf
9731009 <sar.ezaz@aut.ac.ir>
2019-06-23 11:08:34 +0430

    MP3Player created

--- src/Logic/Player/MP3Player.java
+++ src/Logic/Player/MP3Player.java
@@ -13,19 +13,19 @@ import java.io.InputStream;
 
 public class MP3Player implements Runnable {
     private AdvancedPlayer player = null;
-    private Song song;
+    private Song song = null;
     private boolean isPaused = false;
     private FileInputStream inputStream;
 
-    /* public MP3Player(Song song) {
-         super("MP3Player");
-         this.song = song;
-     }*/
+    public void setSong(Song song) {
+        this.song = song;
+    }
+
     @Override
     public void run() {
         try {
             //need chenge
-            inputStream = new FileInputStream("F:\\Jpotify\\src\\Logic\\1.mp3");
+            inputStream = new FileInputStream(song.getDirectory());
             player = new AdvancedPlayer(inputStream);
             while (player.play(1)) {
                 if (isPaused) {
@@ -69,7 +69,7 @@ public class MP3Player implements Runnable {
         synchronized (player) {
             try {
                // player.close();
-                inputStream = new FileInputStream("F:\\Jpotify\\src\\Logic\\1.mp3");
+                inputStream = new FileInputStream(song.getDirectory());
                 player = new AdvancedPlayer(inputStream);
                 player.play(frame, frame + 1);
 
@@ -82,20 +82,4 @@ public class MP3Player implements Runnable {
 
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        MP3Player mp3Player = new MP3Player();
-        Thread t = new Thread(mp3Player);
-        t.start();
-        Thread.sleep(5000);
-        //mp3Player.setPaused();
-        Thread.sleep(3000);
-        mp3Player.seekTo(2000);
-        //mp3Player.setResume();
-        System.out.println(t.isAlive());
-
-
-
-        // mp3Player.setIsPaused();
-
-    }
 }
