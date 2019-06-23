b197a773bb0a51c4b68f6f35b31c9bc94b7fc57e
9731009 <sar.ezaz@aut.ac.ir>
2019-06-23 11:03:54 +0430

    mp3PlayerTest

--- src/Logic/Player/MP3Player.java
+++ src/Logic/Player/MP3Player.java
@@ -4,33 +4,98 @@ import Logic.Song;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 import javazoom.jl.player.advanced.AdvancedPlayer;
+import javazoom.*;
+
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-public class MP3Player {
+public class MP3Player implements Runnable {
     private AdvancedPlayer player = null;
     private Song song;
+    private boolean isPaused = false;
+    private FileInputStream inputStream;
 
-   /* public MP3Player(Song song) {
-        this.song = song;
-    }*/
-    public void playSong(){
+    /* public MP3Player(Song song) {
+         super("MP3Player");
+         this.song = song;
+     }*/
+    @Override
+    public void run() {
         try {
-            FileInputStream inputStream = new FileInputStream("F:\\Jpotify\\src\\Logic\\1.mp3");
+            //need chenge
+            inputStream = new FileInputStream("F:\\Jpotify\\src\\Logic\\1.mp3");
             player = new AdvancedPlayer(inputStream);
-            player.play();
+            while (player.play(1)) {
+                if (isPaused) {
+                    synchronized (player) {
+                        player.wait();
+                    }
+
+                }
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (JavaLayerException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.err.println("wait has problem");
         }
 
     }
 
-    public static void main(String[] args) {
+    /**
+     * This method will pause the music
+     */
+    public void setPaused() {
+        this.isPaused = true;
+    }
+
+    /**
+     * This method will resume the music
+     */
+    public void setResume() {
+        this.isPaused = false;
+        synchronized (player) {
+            player.notifyAll();
+        }
+    }
+
+    /**
+     * This method seek to any frame passed as its parameter
+     */
+    public void seekTo(int frame) {
+        synchronized (player) {
+            try {
+               // player.close();
+                inputStream = new FileInputStream("F:\\Jpotify\\src\\Logic\\1.mp3");
+                player = new AdvancedPlayer(inputStream);
+                player.play(frame, frame + 1);
+
+            } catch (JavaLayerException e) {
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         MP3Player mp3Player = new MP3Player();
-        mp3Player.playSong();
+        Thread t = new Thread(mp3Player);
+        t.start();
+        Thread.sleep(5000);
+        //mp3Player.setPaused();
+        Thread.sleep(3000);
+        mp3Player.seekTo(2000);
+        //mp3Player.setResume();
+        System.out.println(t.isAlive());
+
+
+
+        // mp3Player.setIsPaused();
+
     }
 }
