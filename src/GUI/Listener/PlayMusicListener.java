ed961f23e39b3d8c4f701b67ac1056c2740d3292
mohammadsadra <ms.haeri@aut.ac.ir>
2019-06-26 03:50:40 +0430

    play music button listener created

--- src/GUI/Listener/PlayMusicListener.java
+++ src/GUI/Listener/PlayMusicListener.java
@@ -4,6 +4,7 @@ import Logic.Entities.Library;
 import Logic.Entities.Song;
 import Logic.Player.MP3Player;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -14,10 +15,18 @@ public class PlayMusicListener implements ActionListener {
     private Library library;
     private static MP3Player mp3Player;
     private static Thread tr;
+    private JButton artwork;
+    private JLabel artworkMusicName;
+    private JLabel artworkArtistiName;
+    private JFrame frame;
 
-    public PlayMusicListener(Song song, Library library) {
+    public PlayMusicListener(Song song, Library library,JButton artwork, JLabel artworkMusicName, JLabel artworkArtistiName, JFrame frame) {
         this.song = song;
         this.library = library;
+        this.artwork = artwork;
+        this.artworkMusicName = artworkMusicName;
+        this.artworkArtistiName = artworkArtistiName;
+        this.frame = frame;
         playingSongs = new ArrayList<>();
 
     }
@@ -36,13 +45,31 @@ public class PlayMusicListener implements ActionListener {
         System.out.println(index);
         for (int i = index; i < library.getSongs().size(); i++)
             playingSongs.add(library.getSongs().get(i));
-        if (tr != null)
+        if (tr != null){
             mp3Player.setFalseisRunning();
-        mp3Player = new MP3Player(playingSongs, library);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
+        mp3Player = new MP3Player(playingSongs, library, artwork,artworkMusicName,artworkArtistiName);
 
 
         tr = new Thread(mp3Player);
         tr.start();
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+        System.out.println(mp3Player.getPlayingSong());
+//        artworkMusicName.setText(mp3Player.getPlayingSong().getName());
+//        artworkArtistiName.setText(mp3Player.getPlayingSong().getArtistName());
+//        artwork.setIcon(mp3Player.getPlayingSong().getArtWork());
+
 
     }
 
